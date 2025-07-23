45e5495600ada2a6c69819b3dec5cfb34af4cedb
chriskinal <118364224+chriskinal@users.noreply.github.com>
2025-07-23 11:59:26 -0500

    Bumped version number and removed old HEX versions.

--- lib/aio_system/Version.h
+++ lib/aio_system/Version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 // AiO New Dawn firmware version
-#define FIRMWARE_VERSION "0.1.33-alpha"
+#define FIRMWARE_VERSION "0.1.34-alpha"
 
 // Teensy board type for OTA identification
 #define TEENSY_BOARD_TYPE "Teensy41"
