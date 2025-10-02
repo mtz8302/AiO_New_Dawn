7125beb5f931e085463f2d6cbed2bfea42a21f0f
chriskinal <118364224+chriskinal@users.noreply.github.com>
2025-10-02 16:40:20 -0500

    Added web log viewer.

--- lib/aio_system/Version.h
+++ lib/aio_system/Version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 // AiO New Dawn firmware version
-#define FIRMWARE_VERSION "1.0.71-beta" // Add Lindner tractor CAN support - unified CAN complete (9/9 brands)
+#define FIRMWARE_VERSION "1.0.72-beta" // Add Lindner tractor CAN support - unified CAN complete (9/9 brands)
 
 // Teensy board type for OTA identification
 #define TEENSY_BOARD_TYPE "Teensy41"
